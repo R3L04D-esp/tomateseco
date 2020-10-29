ecb686eb610c659d2925a0bae23546beaa0a3af6
R3L04D <70805183+R3L04D-esp@users.noreply.github.com>
2020-10-29 15:21:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,8 +11,13 @@ app.listen(port, () => console.log(`Example app listening at http://localhost:30
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const config = require('./package.json')
+const privateMessage = require('./private-message.js')
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  
+  privateMessage(client, 'ping', 'Pong!')
 });
 
 client.on('message', msg => {
@@ -20,5 +25,6 @@ client.on('message', msg => {
     msg.reply('Hola, soy el bot <@771126105100910602>, estoy aquí para ofrecerte ayuda de todo tipo, te recomiendo el canal <#771285289179152384> para información sobre mi utilidad, y el canal <#756997888214827129> para abrir un ticket en el que los <@&771145930467573802> puedan serte de ayuda :p');
   }
 });
+
 
 client.login('TOKEN');
